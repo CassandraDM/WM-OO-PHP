d52868c811e8995a3e8fd76543d2f4d6ff564fbc
CassandraDM <cassandra.delmas-marchiset@mail-esd.com>
2024-10-07 16:52:06 +0200

    removeProduct

--- Order.php
+++ Order.php
@@ -1,5 +1,9 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 class Order
 {
     private int $id;
@@ -18,25 +22,55 @@ class Order
 
     private string $status;
 
-    //create an order
-    //delete products
-    //add products
-    //choose shipping method
-    //fill shipping address
-    //pay
-    //cancel
 
     public function __construct(string $customerName, array $products)
     {
         $this->id = rand();
+        if (count(value: $products) > 5) {
+            throw new Error('You can\'t order more than 5 products');
+        }
         $this->products = $products;
         $this->totalPrice = count(value: $products) * 5;
+        if ($customerName === 'David Robert') {
+            throw new Error('You are blacklisted');
+        }
         $this->customerName = $customerName;
         $this->createdAt = new DateTime();
         $this->status = 'CART';
 
         echo "Order n°{$this->id} created";
     }
+
+
+    public function removeProduct(string $product)
+    {
+        if (!in_array(needle: $product, haystack: $this->products)) {
+            throw new Error('Product not found');
+        }
+        $key = array_search(needle: $product, haystack: $this->products);
+        unset($this->products[$key]);
+    }
 }
 
 $order = new Order(customerName: 'John Doe', products: ['product1', 'product2', 'product3']);
+
+try {
+    $order2 = new Order(customerName: 'David Robert', products: ['product1', 'product2', 'product3']);
+} catch (Error $e) {
+    echo $e->getMessage();
+}
+
+try {
+    $order3 = new Order(customerName: 'John Doe', products: ['product1', 'product2', 'product3', 'product4', 'product5', 'product6']);
+} catch (Error $e) {
+    echo $e->getMessage();
+}
+
+
+$order->removeProduct(product: 'product2');
+
+try {
+    $order->removeProduct(product: 'product2');
+} catch (Error $e) {
+    echo $e->getMessage();
+}
