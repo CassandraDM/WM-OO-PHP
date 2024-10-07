2136aa78c76bf500fea269d16f0de6258176339a
CassandraDM <cassandra.delmas-marchiset@mail-esd.com>
2024-10-07 16:06:08 +0200

    construct + new order

--- Order.php
+++ Order.php
@@ -2,18 +2,41 @@
 
 class Order
 {
+    private int $id;
 
     private array $products;
 
     private float $totalPrice;
 
-    private int $id;
+    private ?string $shippingMethod;
 
-    private string $shippingMethod;
+    private ?string $shippingAddress;
 
     private string $customerName;
 
-    private string $createdAt;
+    private DateTime $createdAt;
 
     private string $status;
+
+    //create an order
+    //delete products
+    //add products
+    //choose shipping method
+    //fill shipping address
+    //pay
+    //cancel
+
+    public function __construct(string $customerName, array $products)
+    {
+        $this->id = rand();
+        $this->products = $products;
+        $this->totalPrice = count(value: $products) * 5;
+        $this->customerName = $customerName;
+        $this->createdAt = new DateTime();
+        $this->status = 'CART';
+
+        echo "Order n°{$this->id} created";
+    }
 }
+
+$order = new Order(customerName: 'John Doe', products: ['product1', 'product2', 'product3']);
